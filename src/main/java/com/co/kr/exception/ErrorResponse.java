befea4ed3e9bf75106edd463134d542840561e1c
zkdtjs112naver.com <hwangseon@MacBookPro98.local>
2023-03-20 12:25:26 +0900

    commit

--- src/main/java/com/co/kr/exception/ErrorResponse.java
+++ src/main/java/com/co/kr/exception/ErrorResponse.java
@@ -1,5 +1,21 @@
 package com.co.kr.exception;
 
-public class ErrorResponse {
+import org.springframework.http.HttpStatus;
 
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+
+@Getter
+@Setter
+@Builder(builderMethodName = "builder")
+public class ErrorResponse {
+	
+	private Integer result;
+	private String resultDesc;
+	private HttpStatus httpStatus;
+	private String resDate;
+	private String reqNo;
+	
 }
